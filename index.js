7744be528c06044b909791aea4717fbb570bf7a4
Christian Bell <camaribell@yahoo.com>
2023-02-27 22:47:27 -0800

    moved 3 images

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+let direction = null
+let x = 100;
+let y = 250
+
+
+if(direction === 'west') {
+    x = x - 1 
+}
+if(direction === 'north'){
+    y = y + 1
+}
+if(direction === 'east'){
+    x = x + 1
+}
+if(direction === 'south'){
+    y = y + 1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+
 move(character).to(100, 250)
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
@@ -13,3 +34,6 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+
